3a7bd5b0fd4836d7054c6f3d1345ee3a2646f26d
Debosmita Bedajna <bedajnadebosmita@gmail.com>
2024-09-23 12:54:29 +0530

    Added a splash screen before the actual Spinning globe

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
+import React, { useState } from 'react';
+import IntroScreen from './IntroScreen';
 import SpinningGlobe from './SpinningGlobe';
+import './App.css'
 
 function App() {
+  const [isIntroVisible, setIsIntroVisible] = useState(true);
+
+  const handleStart = () => {
+    setIsIntroVisible(false);
+  };
+
   return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+    <>
+      {isIntroVisible ? (
+        <IntroScreen onStart={handleStart} />
+      ) : (
+      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[5, 5, 5]} intensity={1} />
       <Stars radius={100} depth={50} count={5000} factor={7} saturation={0} fade />
       <OrbitControls />
       <SpinningGlobe />
-    </Canvas>
+      </Canvas>
+      )}
+    </>
   );
 }
 
